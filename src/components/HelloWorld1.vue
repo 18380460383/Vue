4ef1edc4654943865314c7c192361bd7d071b514
pengjiajia2018 <18380460383@126.com>
2018-12-13 10:01:27 +0800

    Initial commit

---
+++ src/components/HelloWorld1.vue
@@ -0,0 +1,97 @@
+<template>
+  <div class="hello" id="main">
+    <h1 id="test">{{ msg +ns}}</h1>
+    <h2>Essential Links</h2>
+    <ul>
+      <li>
+        <a
+          href="http://vuejs-templates.github.io/webpack/"
+          target="_blank"
+        >
+          Docs for This Template
+        </a>
+      </li>
+    </ul>
+    <h2>Ecosystem</h2>
+    <ul>
+      <li>
+        <a
+          href="http://router.vuejs.org/"
+          target="_blank"
+        >
+          vue-router
+        </a>
+      </li>
+      <li>
+        <a
+          href="http://vuex.vuejs.org/"
+          target="_blank"
+        >
+          vuex
+        </a>
+      </li>
+      <li>
+        <a
+          href="http://vue-loader.vuejs.org/"
+          target="_blank"
+        >
+          vue-loader
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://github.com/vuejs/awesome-vue"
+          target="_blank"
+        >
+          awesome-vue
+        </a>
+      </li>
+    </ul>
+  </div>
+</template>
+<script>
+let str1 = '{ "name": "json", "sex": "man" }'
+var jso = null
+export default {
+  name: 'HelloWorld1',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js 1',
+      ns: jso
+    }
+  },
+  created: function () {
+    this.getUrl()
+    this.funjson()
+  },
+  methods: {
+    getUrl: function () {
+      console.log('' + this.$route.query.id)
+    },
+    funjson: function () {
+      this.ns = JSON.parse(str1).name
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  h1, h2 {
+    font-weight: normal;
+  }
+
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+
+  a {
+    color: #42b983;
+  }
+</style>
